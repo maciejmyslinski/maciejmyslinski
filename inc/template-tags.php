80e46c7dfb350b2109510c9fa1bc9f795e1224b1
Maciej Myśliński <maciej.myslinski@netguru.co>
2017-04-03 23:09:08 +0200

    Style article list on mobile

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -19,22 +19,17 @@ function maciejmyslinski_posted_on() {
 
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
+		esc_html( get_the_date('M j, Y') ),
 		esc_attr( get_the_modified_date( 'c' ) ),
 		esc_html( get_the_modified_date() )
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'maciejmyslinski' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		esc_html_x( '%s', 'post date', 'maciejmyslinski' ),
+		$time_string
 	);
 
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'maciejmyslinski' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 
 }
 endif;
--- template-parts/content.php
+++ template-parts/content.php
@@ -10,34 +10,43 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="section typeset">
-		<?php
-		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+	<a href="<?php esc_url( get_permalink() )?>" rel="bookmark" class="post__link">
+		<header class="section typeset">
+			<?php
+			if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php maciejmyslinski_posted_on(); ?>
+			</div><!-- .entry-meta -->
+			<?php
+			endif;
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<p><?php maciejmyslinski_posted_on(); ?></p>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
+			if ( is_single() ) :
+				the_title( '<h1 class="post__title">', '</h1>' );
+			else :
+				the_title( '<h2 class="post__title">', '</h2>' );
+			endif;
+
+			if ( has_post_thumbnail() ) {
+				the_post_thumbnail();
+			} ?>
+
+			<p class="post__more">Read more...</p>
+
+		</header>
+	</a>
 
 	<div class="section typeset">
 		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'maciejmyslinski' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+			// the_content( sprintf(
+			// 	/* translators: %s: Name of current post. */
+			// 	wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'maciejmyslinski' ), array( 'span' => array( 'class' => array() ) ) ),
+			// 	the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			// ) );
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'maciejmyslinski' ),
-				'after'  => '</div>',
-			) );
+			// wp_link_pages( array(
+			// 	'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'maciejmyslinski' ),
+			// 	'after'  => '</div>',
+			// ) );
 		?>
 	</div><!-- .entry-content -->
 
